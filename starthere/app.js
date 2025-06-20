d20a26c9ebe650ff5f8c9ec0a72fa52799125523
Connor McLeod <connorcmcleod@outlook.com>
2025-06-20 19:21:47 +0000

    [automatic commit] 2025-06-20 19:21:47 UTC 175.35.3.242

--- starthere/app.js
+++ starthere/app.js
@@ -81,7 +81,7 @@ let db;
       await db.execute(`
         INSERT INTO Dogs (name, size, owner_id)
         VALUES
-        ('Max', 'medium', (SELECT user_id FROM Users WHERE username = 'alice123')),
+        ('Max', 'medium', 3),
         ('Bella', 'small', (SELECT user_id FROM Users WHERE username = 'carol123')),
         ('Graham', 'large', (SELECT user_id FROM Users WHERE username = 'benstilton')),
         ('John', 'small', (SELECT user_id FROM Users WHERE username = 'tenny123')),
