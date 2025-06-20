08ad890616e72a84d7ab76554a2f6363f0d4bc74
Connor McLeod <connorcmcleod@outlook.com>
2025-06-20 20:21:43 +0000

    [automatic commit] 2025-06-20 20:21:43 UTC 175.35.3.242

--- part1/app.js
+++ part1/app.js
@@ -44,8 +44,6 @@ let db;
     `);
 
         // Insert data if table is empty
-
-
         const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
         if (rows[0].count === 0) {
             await db.execute(`
