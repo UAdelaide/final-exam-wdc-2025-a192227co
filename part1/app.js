c730d73c34e004708870d85f589c14e8a8918856
Connor McLeod <connorcmcleod@outlook.com>
2025-06-21 05:21:49 +0000

    [automatic commit] 2025-06-21 05:21:49 UTC 175.35.3.242

--- part1/app.js
+++ part1/app.js
@@ -203,7 +203,8 @@ app.get('/api/walkers/summary', async (req, res) => {
             SELECT
                 user.username AS walker_username,
                 COUNT(DISTINCT rating.rating_id) AS overallRating,
-                ROUND(AVG(rating.))
+                ROUND(AVG(rating.rating), 1) AS averageRating,
+                COUNT(DISTINCT )
 
             FROM Users user
             LEFT JOIN WalkRatings rating ON user.user_id = rating.walker_id
