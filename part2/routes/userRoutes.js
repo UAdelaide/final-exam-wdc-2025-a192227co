de456ac72f6d276537b91e0efdcdcab5dae8890f
Connor McLeod <connorcmcleod@outlook.com>
2025-06-21 07:17:16 +0000

    [automatic commit] 2025-06-21 07:17:16 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.3.242

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -75,7 +75,12 @@ router.get('/dogs', async (req, res) => {
     return res.status(403).json({ error: 'Cant access this' });
   }
 
-  const ownerID = req.session.user.i;
+  const ownerID = req.session.user.id;
+
+  try {
+    const [rows] = await db.query
+
+  }
 
   res.clearCookie('connect.sid');
   res.json({ message: 'Successful log out' });
