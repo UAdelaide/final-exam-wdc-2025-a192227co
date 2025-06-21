c37533651f638f72854623aa54b828ef123bbeac
Connor McLeod <connorcmcleod@outlook.com>
2025-06-21 05:24:12 +0000

    [automatic commit] 2025-06-21 05:24:12 UTC ;; communications error to 208.67.222.222#53: timed out 175.35.3.242

--- part1/app.js
+++ part1/app.js
@@ -125,14 +125,14 @@ let db;
             await db.execute(`
         INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
         VALUES
-        ((SELECT request_id FROM WalkRequests WHERE status = 'completed' AND dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Tim')),
+        ((SELECT request_id FROM WalkRequests WHERE status = 'completed' AND dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Tim') LIMIT 1),
         (SELECT user_id FROM Users WHERE username = 'benstilton'),
         (SELECT user_id FROM Users WHERE username = 'carol123'),
         2,
         'Shocking service'
         ),
 
-        ((SELECT request_id FROM WalkRequests WHERE status = 'completed' AND dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Max')),
+        ((SELECT request_id FROM WalkRequests WHERE status = 'completed' AND dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Max') LIMIT 1),
         (SELECT user_id FROM Users WHERE username = 'benstilton'),
         (SELECT user_id FROM Users WHERE username = 'tenny123'),
         5,
