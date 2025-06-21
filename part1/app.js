26c02b088326399248105e06e788ff70e4e079ea
Connor McLeod <connorcmcleod@outlook.com>
2025-06-21 03:36:55 +0000

    [automatic commit] 2025-06-21 03:36:54 UTC 175.35.3.242

--- part1/app.js
+++ part1/app.js
@@ -23,7 +23,7 @@ let db;
         });
 
         // Create the database if it doesn't exist
-        await connection.query('CREATE DATABASE IF NOT EXISTS DogWalking');
+        await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
         await connection.end();
 
         // Now connect to the created database
@@ -31,7 +31,7 @@ let db;
             host: 'localhost',
             user: 'root',
             password: '',
-            database: 'DogWalking'
+            database: 'DogWalkService'
         });
 
         // Create a table if it doesn't exist
